f9e3dd2396e3ef8a904ac9158ac67b92aca22ba8
Stan Bobovych <stan.bobovych@gmail.com>
2011-06-02 16:19:45 -0500

    Added forgotten header files.
    Change-Id: I494356d2e574dceb5c68c514f066a09aec224821

---
+++ DCS3/h/DCS.hpp
@@ -0,0 +1,15 @@
+/*
+ * DCS.hpp
+ *
+ *  Created on: Jun 2, 2011
+ *      Author: sbobovyc
+ */
+
+#ifndef DCS_HPP_
+#define DCS_HPP_
+
+#include "Blob.hpp"
+#include "Utils.hpp"
+#include "DCS_types.hpp"
+
+#endif /* DCS_HPP_ */
---
+++ DCS3/h/DCS_types.hpp
@@ -0,0 +1,14 @@
+/*
+ * DCS_types.hpp
+ *
+ *  Created on: Jun 2, 2011
+ *      Author: sbobovyc
+ */
+
+#ifndef DCS_TYPES_HPP_
+#define DCS_TYPES_HPP_
+
+typedef std::map<Magick::Color, unsigned long> DCS_histogram;
+typedef std::vector<Magick::Color> DCS_color_list;
+
+#endif /* DCS_TYPES_HPP_ */
