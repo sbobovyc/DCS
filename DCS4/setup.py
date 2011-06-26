9aeed12aaca763c4b1deaffa1e2f8ae1f98355ae
Stan Bobovych <stan.bobovych@gmail.com>
2011-06-26 18:57:32 -0500

    Setup.py added but not tested.
    Change-Id: I42bdf94a8276898b50964705d160ff9e52a37b5a

---
+++ DCS4/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import glob
+import os
+from distutils.core import setup, Extension
+
+setup(name='DCS',
+      version='0.1',
+      description='Dynamic Cammoflauge System',
+      author='Stanislav Bobovych',
+      author_email='stan.bobovych@gmail.com',
+      url='http://sites.google.com/site/sbobovyc/',      
+      packages=glob.glob(os.path.join('DCS')),
+      ext_modules=[Extension('DCS.utils', ['DCS/utils.cpp'], libraries=['noise'])]
+     )
+
+
+
