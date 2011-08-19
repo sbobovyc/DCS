30c3051f2c4fad6c6d4d98363c97c95b9918790e
Stan Bobovych <stan.bobovych@gmail.com>
2011-08-18 21:23:48 -0500

    DCS required some MinGW dlls to fully function. They are now included with the windows_setup.py script in the distribution creation.

--- DCS/src/windows_setup.py
+++ DCS/src/windows_setup.py
@@ -11,15 +11,17 @@ print sys.path
 
 main_path = os.path.join("gui", "main.py")
 lib_path = os.path.join("utils", "build", "libutils.dll")
+libstdcpp = os.path.join("c:\\MinGW\\bin\\", "libstdc++-6.dll")
+libgcc = os.path.join("c:\\MinGW\\bin\\", "libgcc_s_dw2-1.dll")
 
 setup(
     name="DCS v0.1a",
-    version="0.1a",
+    version="0.1",
     description = "Dynamic Camouflage System",
     author="Stanislav Bobovych",
     author_email="stan.bobovych@gmail.com",
     windows = [
           {'icon_resources': [(0, 'dcs.ico')], 'dest_base': 'DCS', 'script': main_path}
     ],
-    data_files = [(lib_path)]
+    data_files = [(lib_path), (libstdcpp), (libgcc)]
 )
