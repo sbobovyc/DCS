68fffeaa5a6ba2ccf70e27e918b22ef110af5865
Stan Bobovych <stan.bobovych@gmail.com>
2011-10-03 20:05:51 -0500

    Icon and library now get copied to dist with dcs.bat

--- DCS/dcs.bat
+++ DCS/dcs.bat
@@ -1,3 +1,5 @@
     set PIP=C:\Python27\pyinstaller-1.5.1\
     python %PIP%pyinstaller.py --onefile --noconsole --icon src\dcs.ico --name DCS -p src\ src\gui\main.py
+    copy src\dcs.ico dist\
+    copy src\utils\build\libutils.dll dist\
 
